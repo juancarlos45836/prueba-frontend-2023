d8387d0c92fdcc9260761b9a6e7a117dca74385c
CharlyCodigo <juancarlos45836@gmail.com>
2023-05-04 07:48:41 -0600

    feat: add function findUniq

--- 1.js
+++ 1.js
@@ -1,14 +1,26 @@
 // Ejercicio 1
 // Dada una matriz de N elementos en la que todos los elementos son iguales excepto uno,
 // crea una función findUniq que retorne el elemento único.
-//Comment for initial comment
+
+//Comentario para mi Initial commit...
 
 function findUniq(array) {
+    const res = array.filter((element,index, array) => {
+        let repeat = 0;
+        for (let i = 0; i < array.length; i++) {
+            if (element===array[i]) {
+                repeat++;
+            }  
+        }
+        if (repeat==1) return element
+    });
+    console.log(res);
+    return(res);
 }
 
 
 /**
  * TEST Ejercicio 1
  */
-findUniq(['12', 10, '12', 11, 1, 11, 10, '12']) // 1
+findUniq(['12', 10, '12', 11, 1, 2, 2, 11, 10, '12']) // 1
 findUniq(['Capitán América', 'Hulk', 'Deadpool', 'Capitán América', 'Hulk', 'Wonder Woman', 'Deadpool', 'Iron Man', 'Iron Man']) // 'Wonder Woman'
