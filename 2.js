5c31f35667026b938e9840e024816f774bcaba46
CharlyCodigo <juancarlos45836@gmail.com>
2023-05-04 07:50:11 -0600

    feat: add function numbersTop

--- 2.js
+++ 2.js
@@ -3,6 +3,30 @@
 // crea una función numbersTop para obtener los tres elementos más repetidos ordenados de forma descendente por número de repeticiones.
 
 function numbersTop(array) {
+    let newArraySorted = array.sort();
+    let uniqArray= [];
+    let numRepeat = [];
+    let cont = 0;
+    let num1 = 0, num2 = 0, num3 = 0;
+    for (let i = 0; i < newArraySorted.length; i++) {
+        if (array[i]===array[i+1]) {
+            cont ++;
+        }else{
+            uniqArray.push(array[i]);
+            numRepeat.push(cont+1);
+            cont=0;
+        }
+    }
+    let resNumRepeatSorted = [...numRepeat];
+    resNumRepeatSorted.sort();
+    let resDescendNumRepeatSorted = resNumRepeatSorted.reverse();
+    for (let i = 0; i < numRepeat.length; i++) {
+        if (resDescendNumRepeatSorted[0] == numRepeat[i]) num1 = uniqArray[i];
+        if (resDescendNumRepeatSorted[1] === numRepeat[i]) num2 = uniqArray[i];
+        if (resDescendNumRepeatSorted[2] === numRepeat[i]) num3 = uniqArray[i];  
+    }
+    console.log([num1,num2,num3]);
+    return [num1,num2,num3];
 }
 
 /**
